ffb64389eb0a74e8cba40a1e348d55a06f34a871
cenkai <cenkai29@qq.com>
2015-07-04 16:19:08 +0800

    sushu_error

--- practice/sushu.py
+++ practice/sushu.py
@@ -1,10 +1,9 @@
-#
+#素数产生，使用filter
 def _int_iter():
 	n=1
 	while  True:
-		n=n+1
+		n+=1
 		yield n
-		pass
 
 def indivisible(n):
 	return lambda x: x%n > 0
@@ -12,15 +11,17 @@ def indivisible(n):
 def primes():
 	it=_int_iter()
 	yield 2
+	n=2
 	while True:
 		it=filter(indivisible(n), it)
 		n=next(it)
+		print n
 		yield n
 
 
 # 打印1000以内的素数:
 for n in primes():
-    if n < 1000:
+    if n < 10:
         print(n)
     else:
         break
